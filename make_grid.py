b3d7c183307d8a5a8964962d24e4bcbe0f7fcf6f
ineska01 <weronika.kotla@gmail.com>
2023-05-10 22:45:39 +0200

    def merge_grid_with_field error

--- make_grid.py
+++ make_grid.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 from shapely.geometry import box
 import fiona
 from grid_engine import get_grid
+import pandas as pd
 
 class GridMaker:
     def __init__(self, dense, input_files):
@@ -11,6 +12,7 @@ class GridMaker:
         self.get_crs = None
         self.upper_left = None
         self.lower_right = None
+        self.merged_geojson = None
         
     def load_geojson(self):
         self.loaded_geojson = gpd.read_file(self.input_files)
@@ -22,12 +24,19 @@ class GridMaker:
         
     def make_grid(self):
         grid = get_grid(self.upper_left, self.lower_right, self.dense)
-        grid.to_file('siatka_18.geojson', driver='GeoJSON')
-    def merge_grid_with_field(self):
-        pass
+        grid.to_file('siatka_22.geojson', driver='GeoJSON')
     
+    def merge_grid_with_field(self):
+       grid = gpd.read_file('siatka_22.geojson')
+       grid.to_crs('EPSG:3857')
+       field = gpd.read_file('probny.geojson')
+       field.to_crs('EPSG:3857')
+       self.merged_geojson = gpd.overlay(grid, field, how='intersection')
+
+#self.merged_geojson = gpd.GeoDataFrame(pd.concat([grid, field], ignore_index=True))
     def save_results(self):
-        pass
+        self.merged_geojson.to_file("merge_3_geojson.geojson", driver='GeoJSON')
+    
    
     def do(self):
         self.load_geojson()
@@ -36,7 +45,7 @@ class GridMaker:
         self.merge_grid_with_field()
         self.save_results()
     
-
 geojson_1 = GridMaker(35 ,"probny.geojson")
 geojson_1.do()
+
    
